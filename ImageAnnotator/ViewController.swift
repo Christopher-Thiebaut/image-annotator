1b752d023bc0ed7ee5872988e30e8e02ef72cd31
Christopher Thiebaut <christopherthiebaut@gmail.com>
2018-06-12 08:24:44 -0400

    Initial Commit

---
+++ ImageAnnotator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageAnnotator
+//
+//  Created by Christopher Thiebaut on 6/12/18.
+//  Copyright © 2018 Christopher Thiebaut. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
