3c4036a5bbcd76f95c9104c7b7442189343548dd
Christopher Thiebaut <christopherthiebaut@gmail.com>
2018-06-12 20:57:35 -0400

    Use can choose a directory.  imageURLS property of ViewController is set based on the contents of the directory that have an extension that is commonly used for images.

--- ImageAnnotator/ViewController.swift
+++ ImageAnnotator/ViewController.swift
@@ -9,6 +9,22 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    private let fileManager = FileManager.default
+    private let imageFileTypes: Set<String> = ["jpg", "png"]
+    
+    private var imagesDirectory: URL? {
+        didSet {
+            guard let imagesDirectory = imagesDirectory else { return }
+            imageURLS = imagesIn(folder: imagesDirectory)
+        }
+    }
+    
+    private var imageURLS: [URL] = [] {
+        didSet {
+            print("\(imageURLS)")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +37,47 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    private func imagesIn(folder url: URL) -> [URL] {
+        do {
+            let directoryContents = try fileManager.contentsOfDirectory(atPath: url.path)
+            let fileURLs = directoryContents
+                .filter({isAnImageFileType(fileName: $0)})
+                .map({url.appendingPathComponent($0)})
+            return fileURLs
+        } catch let error {
+            NSLog("Failed to load contents of directory \(url.path) due to an error: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    private func isAnImageFileType(fileName: String) -> Bool {
+        let fileNameParts = fileName.split(separator: ".")
+        guard fileNameParts.count > 1, let fileExtension = fileNameParts.last else {
+            return false
+        }
+        return imageFileTypes.contains(String(fileExtension))
+    }
 
+}
 
+//MARK: - Main App Menu Actions
+extension ViewController {
+    @IBAction func chooseDirectoryClicked(sender: Any?) {
+        guard let window = view.window else {
+            return
+        }
+        
+        let panel = NSOpenPanel()
+        panel.canChooseDirectories = true
+        panel.canChooseFiles = false
+        panel.allowsMultipleSelection = false
+        
+        panel.beginSheetModal(for: window) {[weak self] (response) in
+            if response == .OK, let selectedFolder = panel.urls.first {
+                self?.imagesDirectory = selectedFolder
+            }
+        }
+    }
 }
 
